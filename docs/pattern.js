a77fef3d5fc49f4c446d83893cc86dd3c1e52911
AntonRzevskiy <31966248+AntonRzevskiy@users.noreply.github.com>
2017-09-18 19:04:54 +0400

    question #1 The second functional framework

--- docs/pattern.js
+++ docs/pattern.js
@@ -16,4 +16,21 @@ $.TableEdid.defaults = {
     methodAfter: function() {
     },
     
+    // functional framework 2
+    _method: function() {
+        if (
+            this.hasOwnProperty('methodBefore') 
+            && typeof this.methodBefore == 'function' 
+            && this.methodBefore() == true
+            ||
+            ! this.hasOwnProperty('methodBefore')
+        ) {
+
+            // logic here
+
+        } 
+        if (this.hasOwnProperty('methodAfter') && typeof this.methodAfter == 'function')
+            this.methodAfter();
+    },
+    
 };
